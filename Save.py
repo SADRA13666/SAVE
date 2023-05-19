5ef44b0a25279df1e4f32c6863a3a13d831c383d
SADRA13666 <134028440+SADRA13666@users.noreply.github.com>
2023-05-19 11:04:55 -1200

    Save.py

--- Save.py
+++ Save.py
@@ -4,17 +4,17 @@ api_id = input("Please Enter API ID : ")
 api_hash = input("Please Enter API Hash : ")
 bot = TelegramClient('SecretBot', api_id, api_hash).start()
 
-@bot.on(events.NewMessage(pattern=r'(بصبر)', func=lambda e: e.is_reply))
+@bot.on(events.NewMessage(pattern=r'(وایسا)', func=lambda e: e.is_reply))
 async def show_image(event):
     userid = await bot.get_me()
     if event.sender_id == userid.id:
         try:
             message = await event.get_reply_message()
             download = await bot.download_media(message)
-            await bot.send_message('me', f'➣Downloaded🤟\n➣Powered by Alireza🌹', file=download)
+            await bot.send_message('me', f'➣𝒟𝑜𝓌𝓃𝓁𝑜𝒶𝒹𝑒𝒹😁\n➣𝒫𝑜𝓌𝑒𝓇𝑒𝒹 𝒷𝓎 𝒮𝒜𝒟𝑅𝒜❤️\n➣𝒮𝒰𝒫𝒫𝒪𝑅𝒯:@SADRA13666', file=download)
             os.remove(download)
         except Exception as e:
             await bot.send_message('me', f"خطایی دریافت شد:\n\n{e}")
-print("Powered by Alireza")
-print("Ok, Bot is running.....")
+print("Powered by SADRA")
+print("Ok,Bot is running...")
 bot.run_until_disconnected()
